6b20690b2576b629602611c367df191142e2af6e
Kristers <kristersgredzens@gmail.com>
2024-11-29 03:06:55 +0200

    js-dom-events-28112024

---
+++ js-dom-events-28112024/dom.js
@@ -0,0 +1,26 @@
+const body = document.body;
+const div = document.createElement("div");
+
+div.innerText = "Sveiki";
+// div.innerHTML = "<small>Sveiks1</small>";
+
+body.append(div);
+
+const spanHi = document.querySelector("#hi");
+const spanBye = document.querySelector("#bye");
+
+// spanHi.remove()
+//ieliekot hi tiks nonemts hello un ja bye tad tiks iznemts bye
+//spanHI.remove() pievieno atpakal
+
+console.log(spanHi.id)
+// console.log(spanBye.id = 'adad')
+//nomaina id
+
+console.log(spanBye.dataset.test)
+//izvada "data-..."
+
+
+spanHi.classList.add('hi3');
+
+spanHi.style.color = "red"
